31099c80de389af9421331f59dac6fceed1f8ad4
skykui <skykui@gmail.com>
2017-09-04 22:04:27 +0800

    1st submit
    no change

---
+++ src/main/java/com/skykui/webdev/mapapp/MapAppApplication.java
@@ -0,0 +1,12 @@
+package com.skykui.webdev.mapapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapAppApplication.class, args);
+	}
+}
