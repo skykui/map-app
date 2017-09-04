f001597853c84076d36adfc5e6155282d82842b0
skykui <skykui@gmail.com>
2017-09-04 22:41:44 +0800

    set basic application properties
    Test Hello world successfully!

--- src/main/java/com/skykui/webdev/mapapp/MapAppApplication.java
+++ src/main/java/com/skykui/webdev/mapapp/MapAppApplication.java
@@ -2,10 +2,17 @@ package com.skykui.webdev.mapapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MapAppApplication {
-
+    @RequestMapping("/")
+    public String index(){
+        return "Hello World!";
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(MapAppApplication.class, args);
 	}
